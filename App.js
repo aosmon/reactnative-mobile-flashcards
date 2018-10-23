2c0f850bce0b24e6aa2130cf7731e5d97de3e61a
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-23 16:04:15 -0400

    feat: Add top tab navigation for DeckList and AddDeck

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import DeckList from './components/DeckList'
+import DeckDetails from './components/DeckDetails'
+import AddDeck from './components/AddDeck'
+import AddQuestion from './components/AddQuestion'
+import { purple, white } from './utils/colors'
+import Quiz from './components/Quiz'
+import { createMaterialTopTabNavigator, StackNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+
+//import { createStore } from 'redux'
+//import { Provider } from 'react-redux'
+//import reducer from './reducers'
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = createMaterialTopTabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+    },
+  },
+ NewDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor:  Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello!</Text>
-      </View>
+//      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <Tabs />
+        </View>
+//      </Provider>
     );
   }
 }
