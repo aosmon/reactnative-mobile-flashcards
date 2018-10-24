7bd3bb23947674a905bc1d5f0350d6393886df51
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-24 09:58:16 -0400

    feat: Rename component AddQuestion to AddCard

---
+++ components/AddCard.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+import { connect } from 'react-redux'
+
+class AddCard extends Component {
+
+	render() {
+
+		return(
+      <View style={styles.container}>
+        <Text>AddQuestion</Text>
+      </View>			
+		)
+	}
+}
+
+function mapStateToProps (state) {
+
+  return {
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default AddCard
+//export default connect(mapStateToProps)(AddQuestion)
