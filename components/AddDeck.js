85924b97ade8bb15497348910057f984e1ec16a5
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-24 16:00:31 -0400

    feat: Create and style components for AddDeck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native'
+import { TextInput } from 'react-native'
 import { connect } from 'react-redux'
 
 class AddDeck extends Component {
 
+	state = {
+		text: ''
+	}
+
+	onChangeText = (text) => {
+		this.setState({text})
+	}
+
 	render() {
 
 		return(
-      <View style={styles.container}>
-        <Text>AddDeck</Text>
-      </View>
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      	<View style={styles.labelContainer}>
+        	<Text style={styles.label}>What is the title of your new deck?</Text>
+        </View>
+        <TextInput 
+        	style={styles.input}
+        	value={this.state.text}
+        	placeholder='Deck Title'
+        	underlineColorAndroid="transparent" 
+        	onChangeText={(text) => this.onChangeText(text)}
+        />
+      </KeyboardAvoidingView>
 		)
 	}
 }
@@ -27,7 +45,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 20,
+    paddingRight: 20
   },
+  labelContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+		fontSize: 50,
+		textAlign: 'center',
+		fontWeight: 'bold'
+  },
+	input: {
+      margin: 15,
+      height: 60,
+      borderColor: '#7a42f4',
+      borderWidth: 1,
+      width: 300,
+      padding: 10,
+      fontSize: 24
+  },
+  submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+  },
+  submitButtonText:{
+      color: 'white'
+  }
 });
 
-export default connect(mapStateToProps)(AddDeck)
+export default AddDeck
+// export default connect(mapStateToProps)(AddDeck)
