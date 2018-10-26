cd69d406118c6b34758ec8df84c22cf751c64596
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-26 12:07:53 -0400

    feat: Create api for AsyncStorage

--- utils/api.js
+++ utils/api.js
@@ -1,13 +1,44 @@
 import { AsyncStorage } from 'react-native'
+import DECKS_STORAGE_KEY from './helpers'
 
 export function getDecks () {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => JSON.parse(results))
 }
 
-export function getDeck (id) {
+export function getDeck (title) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      return data[title]
+    }
 }
 
 export function saveDeckTitle (title) {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+    [title]: { title: title, questions: [] } 
+  }))
 }
 
 export function addCardToDeck (title, card) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      if(data[title]){
+        data[title].questions.push(card);
+        AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
+      }
+    })
+}
+
+export function deleteDeck (title) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      if(data[title]){
+        data[title] = undefined
+        delete data[title]
+        AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
+      }
+    })
 }
