cfb8e7a3eeb666eaad788ca172935762539c4002
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-31 21:26:26 -0400

    feat: Add reducer to delete deck

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions'
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, DELETE_CARD } from '../actions'
 
 function decks (state = {}, action) {
   switch (action.type) {
@@ -23,6 +23,10 @@ function decks (state = {}, action) {
           questions: state[action.title].questions.concat(action.card)
         }
       }
+    case DELETE_CARD :
+      const { [action.title]: title , ...newState } = state
+      return newState
+
     default :
       return state
   }
