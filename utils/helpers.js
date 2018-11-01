9150c8588b473f8e94666f4303d80235294bf877
Aizhan Osmonova <osmonova86@gmail.com>
2018-11-01 09:29:28 -0400

    fix: Fix typo in notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -39,7 +39,7 @@ export function setLocalNotification () {
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
               tomorrow.setHours(12)
-              tomorrow.setMintutes(0)
+              tomorrow.setMinutes(0)
 
               Notifications.scheduleLocalNotificationsAsync(
                 createNotification(),
